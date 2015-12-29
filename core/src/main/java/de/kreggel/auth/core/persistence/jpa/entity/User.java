0b72e419a7f410931ca43245788dbafdc6528b47
OliverKlette85 <Oliverschmidt1985@gmail.com>
2015-12-29 21:56:06 +0100

    created user entity

---
+++ core/src/main/java/de/kreggel/auth/core/persistence/jpa/entity/User.java
@@ -0,0 +1,64 @@
+package de.kreggel.auth.core.persistence.jpa.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by oliverklette on 12/29/15.
+ */
+
+@Entity
+@Table
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+
+    public User(int id, String username, String password, String email) {
+        super( );
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User() {
+        super( );
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
