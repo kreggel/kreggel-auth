7d77c61981566422f99a1c85035ee47b1f0c1f97
OliverKlette85 <Oliverschmidt1985@gmail.com>
2016-01-01 22:59:58 +0100

    add createUser method to Registration class

--- core/src/main/java/de/kreggel/auth/core/api/Registration.java
+++ core/src/main/java/de/kreggel/auth/core/api/Registration.java
@@ -1,4 +1,35 @@
 package de.kreggel.auth.core.api;
 
+import de.kreggel.auth.core.persistence.jpa.entity.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
 public class Registration {
+
+    public void createUser(String username, String password, String email ) {
+
+        EntityManager entityManager = Persistence.createEntityManagerFactory("kreggel-auth").createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        User user = new User();
+
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+
+        entityManager.persist(user);
+
+        entityManager.getTransaction().commit();
+
+        // see that the ID of the user was set by Hibernate
+        System.out.println("user=" + user + ", user.id=" + user.getId());
+
+        User foundUser = entityManager.find(User.class, user.getId());
+
+        System.out.println("foundUser=" + foundUser);
+
+        entityManager.close();
+    }
 }
